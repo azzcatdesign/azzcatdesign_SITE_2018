3cb6b976eb3f737e5fbe0fdf902b460af2a95b06
Catherine Azzarello <azzcatdesign@gmail.com>
2018-09-13 17:01:04 -0700

    All. The. Things. Mostly struggled with js for the return to top button. Site now live.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,12 @@
-// @codekit-prepend quiet "../../bower_components/jquery/src/jquery.js", 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {    // If page is scrolled more than 50px
+        $('#top').fadeIn("slow");       // Fade in the arrow
+    } else {
+        $('#top').fadeOut("slow");      // Else fade out the arrow
+    }
+});
+$('#top').click(function() {            // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                   // Scroll to top of body
+    }, 50);
+});
